6ea1d92be24bc8aa4c5f68918472480b9d55ce9d
mohamedaboeldahab <108568318+mohamedaboeldahab@users.noreply.github.com>
2026-04-30 01:00:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+window.auth = auth;   // جعل auth متاحًا عالميًا
 const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
