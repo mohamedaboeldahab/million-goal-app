e70d7ce0a12d136324b2298f14bfff6d331ffe3b
mohamedaboeldahab <108568318+mohamedaboeldahab@users.noreply.github.com>
2026-04-29 09:59:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,3 @@
-// sw.js - إصدار آمن لـ Vercel
 self.addEventListener('install', event => {
   self.skipWaiting();
 });
@@ -7,3 +6,7 @@ self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
 });
 
+// ✅ هذا المستمع ضروري لاعتبار الموقع تطبيقاً قابلاً للتثبيت
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
