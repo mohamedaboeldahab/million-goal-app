0d9b40310fb3b3090f92948be1b7053e3673e7a3
mohamedaboeldahab <108568318+mohamedaboeldahab@users.noreply.github.com>
2026-05-02 11:30:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,4 @@
-const CACHE = 'shark-fast-v2';
-const ASSETS = [
-  '/', '/index.html', '/app.js', '/home.html', '/profile.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-    )).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
+const CACHE = 'shark-fast-v3';
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/','/index.html','/app.js','/home.html']))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k => Promise.all(k.filter(x => x !== CACHE).map(x => caches.delete(x)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
