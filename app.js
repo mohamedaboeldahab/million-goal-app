d39363b6450e2a94a20877bfa7a2f07f3145ee2c
mohamedaboeldahab <108568318+mohamedaboeldahab@users.noreply.github.com>
2026-05-03 11:23:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -680,5 +680,5 @@ window.engine = {
         input.value = '';
     }
 };
-window.__firestore_helpers = { collection, query, orderBy, getDocs, addDoc, serverTimestamp };
+window.__firestore_helpers = { collection, query, orderBy, getDocs, addDoc, serverTimestamp, doc, getDoc };
 window.engine.init();
